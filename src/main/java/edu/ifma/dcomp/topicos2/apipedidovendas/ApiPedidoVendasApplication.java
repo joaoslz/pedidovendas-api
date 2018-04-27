446d5598ffb60ad972a9b28b71baf4787ea33828
joaoslz <joaoslz@gmail.com>
2018-04-27 00:03:42 -0300

    Primeira versão do projeto

---
+++ src/main/java/edu/ifma/dcomp/topicos2/apipedidovendas/ApiPedidoVendasApplication.java
@@ -0,0 +1,12 @@
+package edu.ifma.dcomp.topicos2.apipedidovendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPedidoVendasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPedidoVendasApplication.class, args);
+    }
+}
